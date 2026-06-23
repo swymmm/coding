78613d561c08400dc5342e67a0bff07333a2aa9a
swymmm <swayamkhade5005@gmail.com>
2026-06-23 14:15:37 +0530

    Add list example

---
+++ python/week 2/day 9/list.py
@@ -0,0 +1,17 @@
+fruits=["apple", "banana", "orange", "mango", "grape"]
+scores=[44,23,56,43,65]
+mixed=["apple", 44, True, 23]
+empty=[]   #empty list
+
+#for lenght
+print(len(fruits))
+
+print(fruits[0])  #first element
+print(fruits[1])  #second element
+print(fruits[-1]) #last element
+print(fruits[-2]) #second last element
+
+#slicing [start:stop]  stop is excluded
+print(fruits[1:3])
+print(fruits[:2])
+print(fruits[1:])
