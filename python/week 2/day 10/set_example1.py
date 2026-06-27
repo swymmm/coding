67994b84bf6da685cf0af14f131245bb1a69949d
swymmm <swayamkhade5005@gmail.com>
2026-06-27 11:44:42 +0530

    Add tuple unpacking example script

---
+++ python/week 2/day 10/set_example1.py
@@ -0,0 +1,4 @@
+#Create a tuple for your top 3 movies. Unpack into 3 variables. Print them.
+movies=("The Godfather","The Dark Knight","Inception","Pulp Fiction")
+a, b, c, d=movies
+print(f"1. {a}\n2. {b}\n3. {c}\n4. {d}")
